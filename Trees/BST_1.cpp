4da23caf400b327215bd410146e487451e546fb4
Md. Shamsul Arefin <mdshamsularefin707@gmail.com>
2026-06-19 19:51:18 +0600

    Implement recursive Binary Search Tree in C++

---
+++ Trees/BST_1.cpp
@@ -0,0 +1,70 @@
+//Recursive implementation of Binary Search Tree (BST) in C++
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+struct Node{
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) : data(val), left(nullptr), right(nullptr){}
+};
+
+Node* insert(Node* root, int val){
+    if(root == nullptr){
+        return new Node(val);
+    }
+
+    if(val < root-> data){
+        root->left = insert(root->left, val);
+    } else {
+        root->right = insert(root->right, val);
+    }
+
+    return root;
+}
+
+
+void printTreeVisual(Node* root, int space = 0, int height = 5) {
+    // Base case
+    if (root == nullptr) return;
+
+    // Increase distance between levels
+    space += height;
+
+    // Process right child first (prints at the top)
+    printTreeVisual(root->right, space);
+
+    // Print current node after spaces
+    cout << endl;
+    for (int i = height; i < space; i++) {
+        cout << " ";
+    }
+    cout << root->data << "\n";
+
+    // Process left child (prints at the bottom)
+    printTreeVisual(root->left, space);
+}
+
+Node* constructBSTFromArray(int arr[], int n){
+    Node* root = nullptr;
+    for(int i = 0; i < n; i++){
+        root = insert(root, arr[i]);
+    }
+    return root;
+}
+
+int main(){
+
+    int values[] = {50, 30, 70, 20, 40, 60, 80};
+
+    Node* root = constructBSTFromArray(values, sizeof(values)/sizeof(values[0]));
+    cout << "Binary Search Tree constructed successfully\n";
+    cout << endl;
+    printTreeVisual(root);
+
+    return 0;
+}
